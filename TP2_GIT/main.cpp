86d24970ae0404671fa6a1433f7e26841250c0d5
user <user@user-VirtualBox>
2016-11-24 08:55:49 +0100

    bonjour !

---
+++ TP2_GIT/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 24 novembre 2016, 08:54
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "hell world !!!!!" << endl;
+    return 0;
+}
+
