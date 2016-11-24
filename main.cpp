9b04c97efced1585ca3ca6bc1ff57d7b404c2a4d
gjrvb114 <gjrvb114@gmail.com>
2016-11-24 09:27:17 +0100

    test commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: eleve
@@ -19,4 +13,3 @@ int main() {
     cout << "Hello World !" << endl;
     return 0;
 }
-
