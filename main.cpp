80c9dade437c2d6cab23178023cbca891c52be18
gjrvb114 <gjrvb114@gmail.com>
2016-11-24 08:45:09 +0100

    Création main - test

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 24 novembre 2016, 08:03
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello World !" << endl;
+    return 0;
+}
+
